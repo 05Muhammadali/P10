cce5287eeea6e0d78b81ccde57a2101c35e47a68
mr.Richard <123379322+05Muhammadali@users.noreply.github.com>
2023-02-03 15:22:29 +0500

    TASKS #26

--- Test/task_4.py
+++ Test/task_4.py
@@ -3,6 +3,7 @@ from pytube import YouTube
 link = input("Send yourube video link:")
 video = YouTube(link)
 quality = input("Quality video: (High/Low)")
+result = None
 if quality == "High":
     result = video.streams.get_highest_resolution()
 if quality == "Low":
